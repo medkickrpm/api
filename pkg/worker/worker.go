5787a8c361f8e709d1e1842504ba1bf390342339
Mir Shahriar Sabuj <shahriar@Mirs-MacBook-Pro.local>
2023-11-14 14:47:46 +0600

    updated sql

--- pkg/worker/worker.go
+++ pkg/worker/worker.go
@@ -17,7 +17,8 @@ func ProcessCPTCode99453() error {
 		Where("services.is_enabled = ?", true).
 		Where("services.code = ?", "RPM").
 		Where("patient_services.ended_at IS NULL").
-		Joins("LEFT JOIN last_bill_entries ON last_bill_entries.patient_id = patient_services.patient_id").Where("last_bill_entries.c99453 = 0 OR last_bill_entries.c99453 IS NULL")
+		Joins("LEFT JOIN last_bill_entries ON last_bill_entries.patient_id = patient_services.patient_id").
+		Where("last_bill_entries.c99453 = 0 OR last_bill_entries.c99453 IS NULL")
 
 	if err := db.Pluck("patient_services.patient_id", &patientList).Error; err != nil {
 		return err
@@ -33,7 +34,7 @@ func ProcessCPTCode99453() error {
 	db = database.DB.Model(&models.Device{}).Joins("JOIN device_telemetry_data ON device_telemetry_data.device_id = devices.id").
 		Where("devices.user_id IN (?)", patientList).
 		Where("device_telemetry_data.measured_at < ?", time.Now().UTC().AddDate(0, 0, -16)).
-		Group("devices.id")
+		Group("devices.user_id")
 
 	if err := db.Pluck("devices.user_id", &filteredPatientList).Error; err != nil {
 		return err
