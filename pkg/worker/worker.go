ba9579d1728c9936667fb1b9a0edc60f8aad3fb2
Mir Shahriar Sabuj <shahriar@Mirs-MacBook-Pro.local>
2023-11-16 08:03:28 +0600

    updated job time

--- pkg/worker/worker.go
+++ pkg/worker/worker.go
@@ -46,57 +46,59 @@ func RunCPTWorkerForPatient(patientID uint) {
 }
 
 func RunCPTWorker() {
-	s := gocron.NewScheduler(time.UTC)
+	loc, _ := time.LoadLocation("EST")
 
-	job99453, _ := s.Tag("99453").Every(1).Day().At("05:30").Do(func() {
+	s := gocron.NewScheduler(loc)
+
+	job99453, _ := s.Tag("99453").Every(1).Day().At("23:30").Do(func() {
 		if err := processCPTCode99453(); err != nil {
 			fmt.Println(err)
 		}
 	})
 
-	job99454, _ := s.Tag("99454").Cron("30 5 16-31 * *").Do(func() {
+	job99454, _ := s.Tag("99454").Cron("30 23 16-31 * *").Do(func() {
 		if err := processCPTCode99454(); err != nil {
 			fmt.Println(err)
 		}
 	})
 
-	job99457, _ := s.Tag("99457").Every(1).Day().At("05:30").Do(func() {
+	job99457, _ := s.Tag("99457").Every(1).Day().At("23:30").Do(func() {
 		if err := processCPTCode99457(); err != nil {
 			fmt.Println(err)
 		}
 	})
 
-	job99458, _ := s.Tag("99458").Every(1).Day().At("05:30").Do(func() {
+	job99458, _ := s.Tag("99458").Every(1).Day().At("23:30").Do(func() {
 		if err := processCPTCode99458(); err != nil {
 			fmt.Println(err)
 		}
 	})
 
-	job99490, _ := s.Tag("99490").Every(1).Day().At("05:30").Do(func() {
+	job99490, _ := s.Tag("99490").Every(1).Day().At("23:30").Do(func() {
 		if err := processCPTCode99490(); err != nil {
 			fmt.Println(err)
 		}
 	})
 
-	job99439, _ := s.Tag("99439").Every(1).Day().At("05:30").Do(func() {
+	job99439, _ := s.Tag("99439").Every(1).Day().At("23:30").Do(func() {
 		if err := processCPTCode99439(); err != nil {
 			fmt.Println(err)
 		}
 	})
 
-	job99426, _ := s.Tag("99426").Every(1).Day().At("05:30").Do(func() {
+	job99426, _ := s.Tag("99426").Every(1).Day().At("23:30").Do(func() {
 		if err := processCPTCode99426(); err != nil {
 			fmt.Println(err)
 		}
 	})
 
-	job99427, _ := s.Tag("99427").Every(1).Day().At("05:30").Do(func() {
+	job99427, _ := s.Tag("99427").Every(1).Day().At("23:30").Do(func() {
 		if err := processCPTCode99427(); err != nil {
 			fmt.Println(err)
 		}
 	})
 
-	job99484, _ := s.Tag("99484").Every(1).Day().At("05:30").Do(func() {
+	job99484, _ := s.Tag("99484").Every(1).Day().At("23:30").Do(func() {
 		if err := processCPTCode99484(); err != nil {
 			fmt.Println(err)
 		}
