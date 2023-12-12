864931b9630839d7012a0c9206179968ead7ef6d
Mazhar Hussain <mazhar@air.business>
2023-12-12 22:39:55 +0100

    Remove unused code

--- utils/utils.go
+++
@@ -1,9 +0,0 @@
-package utils
-
-// make ternary operator utility function for golang
-func TernaryIF(condition bool, a, b interface{}) *interface{} {
-	if condition {
-		return &a
-	}
-	return &b
-}
