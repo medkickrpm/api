4184e0a77620ffe8e14278e82b81dd64270c768f
Mazhar Hussain <mazhar@air.business>
2023-12-21 23:38:08 +0100

    Add roles decoration

--- v1/organization/main.go
+++ v1/organization/main.go
@@ -8,21 +8,21 @@ import (
 
 func Routes(r *echo.Group) {
 	r.POST("/organization", createOrganization, middleware.NotGuest, middleware.HasRole("admin"))
-	r.GET("/organization/:id", getOrganization, middleware.NotGuest)
-	r.PATCH("/organization/:id", updateOrganization, middleware.NotGuest, middleware.HasRole("nurse", "doctor", "admin"))
+	r.GET("/organization/:id", getOrganization, middleware.NotGuest, middleware.HasRole("admin", "org_admin", "care_manager"))
+	r.PATCH("/organization/:id", updateOrganization, middleware.NotGuest, middleware.HasRole("admin"))
 	r.DELETE("/organization/:id", deleteOrganization, middleware.NotGuest, middleware.HasRole("admin"))
 
 	r.GET("/organization/:id/devices", getDevicesInOrganization, middleware.NotGuest, middleware.HasRole("nurse", "doctor", "admin"))
 
-	r.PUT("/organization/:id/interaction-setting", upsertInteractionSetting, middleware.NotGuest, middleware.HasRole("doctor", "admin"))
-	r.GET("/organization/:id/interaction-setting", getInteractionSetting, middleware.NotGuest, middleware.HasRole("nurse", "doctor", "admin"))
+	r.PUT("/organization/:id/interaction-setting", upsertInteractionSetting, middleware.NotGuest, middleware.HasRole("admin", "org_admin", "care_manager", "patient"))
+	r.GET("/organization/:id/interaction-setting", getInteractionSetting, middleware.NotGuest, middleware.HasRole("admin", "org_admin", "care_manager", "patient"))
 
-	r.GET("/organization/:id/telemetry-alert", listTelemetryAlert, middleware.NotGuest, middleware.HasRole("nurse", "doctor", "admin"))
-	r.PATCH("/organization/:id/telemetry-alert/:alert/resolve", resolvedTelemetryAlert, middleware.NotGuest, middleware.HasRole("nurse", "doctor", "admin"))
+	r.GET("/organization/:id/telemetry-alert", listTelemetryAlert, middleware.NotGuest, middleware.HasRole("admin", "org_admin", "care_manager"))
+	r.PATCH("/organization/:id/telemetry-alert/:alert/resolve", resolvedTelemetryAlert, middleware.NotGuest, middleware.HasRole("admin", "org_admin", "care_manager"))
 
-	r.GET("/organization/:id/billing-report", getBillingReport, middleware.NotGuest, middleware.HasRole("admin"))
+	r.GET("/organization/:id/billing-report", getBillingReport, middleware.NotGuest, middleware.HasRole("admin", "org_admin", "care_manager"))
 
-	r.GET("/diagnoses", listDiagnosisCodes, middleware.NotGuest, middleware.HasRole("nurse", "doctor", "admin"))
+	r.GET("/diagnoses", listDiagnosisCodes, middleware.NotGuest, middleware.HasRole("admin", "org_admin", "care_manager", "patient"))
 
 	r.GET("/services", listServices, middleware.NotGuest, middleware.HasRole("nurse", "doctor", "admin"))
 }
